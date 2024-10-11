bc6ae72353631572b883c12ff48f1d8cbe89e79c
SrivastavaD <sdevansh296@gmail.com>
2024-10-11 17:00:42 +0530

    Adding JS DOM Concepts.

---
+++ JSDOM/JSDOM 2/index.js
@@ -0,0 +1,104 @@
+
+// function eventFunction() {
+//     console.log('I have clicked on the docuemnt');
+// }
+
+// document.addEventListener('click', eventFunction);
+
+// document.removeEventListener('click', eventFunction);
+
+// const content = document.querySelector('#wrapper');
+
+// content.addEventListener('click', function(babbar) {
+//     console.log(babbar);
+// })
+
+// let links = document.querySelectorAll('a');
+// let thirdLink = links[2];
+
+// thirdLink.addEventListener('click', function(event) {
+//     event.preventDefault();
+//     console.log('maza aaya, accha laga');
+// });
+
+// let myDiv = document.createElement('div');
+
+// function paraStatus(event) {
+//     console.log('Para ' + event.target.textContent);
+// } 
+// myDiv.addEventListener('click', paraStatus);
+
+// for(let i=1; i<=100; i++) {
+//     let newElement = document.createElement('p');
+//     newElement.textContent = 'This is para ' + i;
+
+//     myDiv.appendChild(newElement);
+// }
+// document.body.appendChild(myDiv);
+
+// let element = document.querySelector('#wrapper');
+
+// element.addEventListener('click', function(event) {
+//     if(event.target.nodeName === 'SPAN') {
+//         console.log('span pr click kia hai' + event.target.textContent);
+//     }
+// });
+
+// function changeText(){
+//     let fpara = document.getElementById('fpara');
+// fpara.textContent="Hello Devansh"
+
+// }
+
+// let fpara =  document.getElementById('fpara');
+// fpara.addEventListener('click', changeText);
+
+
+// Code 1 
+
+const t1 = performance.now()
+
+for(let i =1; i<100; i++){
+    let para = document.createElement('p');
+    para.textContent = "This is para" +i;
+    document.body.appendChild(para);
+}
+
+const t2 = performance.now();
+
+console.log("Total time by code 1: " + (t2-t1));
+
+
+//code 2
+
+const t3 = performance.now()
+
+let mydiv = document.createElement('div');
+
+for(let i = 1; i<=100; i++) {
+    let para = document.createElement('p');
+    para.textContent = "This is Para" +i;
+    myDiv.appendChild(para);
+}
+
+document.body.appendChild(mydiv);
+
+const t4 = performance.now();
+
+console.log("Total time by code 2: " + (t4-t3));
+
+
+// best code
+
+let fragment = document.createDocumentFragment();
+
+for(let i = 1; i<100; i++){
+    let para  = document.createElement('p');
+    para.textContent = "This is para" +i;
+
+    //NO reflow and repaint for the below line
+    fragment.appendChild(para);
+
+}
+
+document.body.appendChild(fragment);

1cee3a0f9c855ed0b68fa3c244c337c2b2744729
SrivastavaD <sdevansh296@gmail.com>
2024-12-04 20:47:53 +0530

    some basic concepts.

---
+++ JavaScript/JSBasics_5/index.js
@@ -0,0 +1,86 @@
+// Some basics of Javascript
+
+//to find the sum
+
+// function sumNumbers(num1, num2){
+//     const sum = num1 + num2 ;
+//     return sum;
+
+// }
+
+// console.log(sumNumbers(19,18));
+
+// Write a function that takes a strings as an arguments and returns its length.
+// function getStringsLength(str){
+//     const length = str.length;  //get the length of the strings using the .length property.
+//     return length;
+
+// }
+
+// console.log(getStringsLength("Devansh"));
+// console.log(getStringsLength("ThalaForaReason"));
+
+//write a function that two numbers and displays their sum, difference , product, and qutioent.
+
+// function calNumbers(num1, num2) {
+//   const sum = num1 + num2;
+//   const diff = num1 - num2;
+
+//   const mul = num1 * num2;
+//   const div = num1 / num2;
+
+//   console.log("sum:", sum);
+//   console.log("diff", diff);
+//   console.log("mul:", mul);
+//   console.log("div:", div);
+
+//   console.log(calNumbers(5, 10));
+// }
+
+// const num1 = Number(22);
+// const num2 = Number(43);
+
+// const sum = num1 + num2;
+// const diff = num1 - num2;
+// const mul= num1 * num2;
+// const div= num1 / num2;
+
+// console.log(`sum: ${sum}`);
+// console.log(`diff: ${diff}`);
+// console.log(`mul: ${mul}`);
+// console.log(`div: ${div}`);
+
+//Write a program that displays a strings in reverse order.
+// const originalString = "Codehelp";
+
+// function reverseString(str){
+//     const reverseString = str.split("").reverse().join("");
+//     return reverseString;
+// }
+
+// console.log(reverseString(originalString));
+
+// Define a function that takes a number parameter
+function checkNumber(number) {
+    // Check if the number is positive
+    if (number > 0) {
+      console.log(`${number} is positive`);
+    }
+    // Check if the number is negative
+    else if (number < 0) {
+      console.log(`${number} is negative`);
+    }
+    // If neither of the above conditions are met, the number must be zero
+    else {
+      console.log(`${number} is zero`);
+    }
+  }
+  
+  // Call the function with various test cases to see the output
+  checkNumber(10); // Output: "10 is positive"
+  checkNumber(-5); // Output: "-5 is negative"
+  checkNumber(0); // Output: "0 is zero"
+  checkNumber(1); // Output: "1 is positive"
+  checkNumber(-1); // Output: "-1 is negative"
+
+
